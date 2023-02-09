7370fb14f4ab16ea28f8d9b06f82d3eb9aeac0b8
Cristofer-Bonmann <cristoferbonmann@gmail.com>
2023-02-08 23:02:02 -0300

    Adicionado documentação para os métodos da classe AlturaRenderer.

--- src/main/java/nfmanualkit/util/AlturaRenderer.java
+++ src/main/java/nfmanualkit/util/AlturaRenderer.java
@@ -4,16 +4,48 @@ import java.awt.*;
 import javax.swing.JTable;
 import javax.swing.table.TableCellRenderer;
 
-// TODO: 08/02/2023 inserir doc
+/**
+ * Classe responsável por redimensionar a altura das linha de um componente {@link JTable} dinâmicamente.
+ */
 public class AlturaRenderer implements TableCellRenderer {
   private final JTable table;
 
-  // TODO: 08/02/2023 inserir doc
+  /**
+   * Construtor da classe. Recebe o componente que será renderizado.
+   * @param table componente {@link JTable}.
+   */
   public AlturaRenderer(JTable table) {
     this.table = table;
   }
 
-  // TODO: 08/02/2023 inserir doc
+  /**
+   * Renderiza a altura da linha do componente {@link JTable}. A altura adotada será a maior altura encontrada nas células
+   * da linha. <br>
+   * JavaDoc original do {@link TableCellRenderer}:<br>
+   *
+   * @param table           the <code>JTable</code> that is asking the
+   *                          renderer to draw; can be <code>null</code>
+   * @param value           the value of the cell to be rendered.  It is
+   *                          up to the specific renderer to interpret
+   *                          and draw the value.  For example, if
+   *                          <code>value</code>
+   *                          is the string "true", it could be rendered as a
+   *                          string or it could be rendered as a check
+   *                          box that is checked.  <code>null</code> is a
+   *                          valid value
+   * @param isSelected      true if the cell is to be rendered with the
+   *                          selection highlighted; otherwise false
+   * @param hasFocus        if true, render cell appropriately.  For
+   *                          example, put a special border on the cell, if
+   *                          the cell can be edited, render in the color used
+   *                          to indicate editing
+   * @param row             the row index of the cell being drawn.  When
+   *                          drawing the header, the value of
+   *                          <code>row</code> is -1
+   * @param column          the column index of the cell being drawn
+   *
+   * @return Component
+   */
   public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
 
     // Obtém o renderizador padrão da célula
@@ -29,7 +61,13 @@ public class AlturaRenderer implements TableCellRenderer {
     return c;
   }
 
-  // TODO: 08/02/2023 inserir doc
+  /**
+   * Redimensiona a altura das células de todas as linhas.
+   * <p>
+   * Percorre todas as linhas de um componente {@link JTable}. Para cada linha será identificada qual é a célula com a
+   * maior altura e aplica essa altura para todas as cálulas da linha.
+   * </p>
+   */
   public void adjustRowHeights() {
 
     for (int row = 0; row < table.getRowCount(); row++) {
