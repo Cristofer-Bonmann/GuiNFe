1306ca3e798739c0def627397a657728563cf319
Cristofer-Bonmann <cristoferbonmann@gmail.com>
2023-02-09 22:47:17 -0300

    Adicionado documentação.

--- src/main/java/nfmanualkit/util/TableCellRendererHeader.java
+++ src/main/java/nfmanualkit/util/TableCellRendererHeader.java
@@ -5,10 +5,17 @@ import javax.swing.table.JTableHeader;
 import javax.swing.table.TableCellRenderer;
 import java.awt.*;
 
-// TODO: 09/02/2023 inserir doc
+/**
+ * Classe responsável por renderizar a JTable de função 'header'.
+ */
 public class TableCellRendererHeader extends JLabel implements TableCellRenderer {
 
-  // TODO: 09/02/2023 inserir doc
+  /**
+   * Contrutor da classe. Recebe um componente JTable para parâmetro e as propriedades 'foreground', 'background' e 'font'
+   *  são aplicadas neste renderer.
+   *
+   * @param table componente {@link JTable};
+   */
   public TableCellRendererHeader(JTable table) {
     JTableHeader header = table.getTableHeader();
     setOpaque(true);
@@ -18,7 +25,33 @@ public class TableCellRendererHeader extends JLabel implements TableCellRenderer
     setFont(header.getFont());
   }
 
-  // TODO: 09/02/2023 inserir doc
+  /**
+   * <p>
+   *   Documentação original:
+   * </p>
+   * @param table           the <code>JTable</code> that is asking the
+   *                          renderer to draw; can be <code>null</code>
+   * @param value           the value of the cell to be rendered.  It is
+   *                          up to the specific renderer to interpret
+   *                          and draw the value.  For example, if
+   *                          <code>value</code>
+   *                          is the string "true", it could be rendered as a
+   *                          string or it could be rendered as a check
+   *                          box that is checked.  <code>null</code> is a
+   *                          valid value
+   * @param isSelected      true if the cell is to be rendered with the
+   *                          selection highlighted; otherwise false
+   * @param hasFocus        if true, render cell appropriately.  For
+   *                          example, put a special border on the cell, if
+   *                          the cell can be edited, render in the color used
+   *                          to indicate editing
+   * @param row             the row index of the cell being drawn.  When
+   *                          drawing the header, the value of
+   *                          <code>row</code> is -1
+   * @param column          the column index of the cell being drawn
+   *
+   * @return
+   */
   @Override
   public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
                                                  int row, int column) {
