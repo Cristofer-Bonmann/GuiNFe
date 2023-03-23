e1ca0b50022056adc38b760fd4297dbb449c2eae
Cristofer-Bonmann <cristoferbonmann@gmail.com>
2023-03-23 17:27:33 -0300

    Refimaneto na interface gráfica da janela 'Sobre'.

--- src/main/java/nfmanualkit/main/JDSobre.java
+++ src/main/java/nfmanualkit/main/JDSobre.java
@@ -16,6 +16,14 @@ public class JDSobre extends JDialog {
 
     initComponents();
     inits();
+    initEvents();
+  }
+
+  private void initEvents() {
+
+    jbOk.addActionListener(actionEvent -> {
+      dispose();
+    });
   }
 
   private void inits() {
@@ -30,20 +38,35 @@ public class JDSobre extends JDialog {
 
   private JLabel jlSobre;
 
+  private JButton jbOk;
+
   private void initComponents() {
-    final Dimension dimension = new Dimension(600, 500);
+    final Dimension dimension = new Dimension(600, 250);
 
     setDefaultCloseOperation(DISPOSE_ON_CLOSE);
     setSize(dimension);
     setPreferredSize(dimension);
     setLocationRelativeTo(null);
+    setResizable(false);
+    setTitle(Recursos.get("jdsobre_titulo"));
 
     setLayout(new BorderLayout());
 
     final JPanel jpSobre = new JPanel(new FlowLayout());
+    jpSobre.setBackground(Color.WHITE);
     jlSobre = new JLabel("");
+    jlSobre.setBackground(Color.WHITE);
     jpSobre.add(jlSobre);
 
+    final JPanel jpOk = new JPanel();
+    jpOk.setLayout(new FlowLayout());
+
+    jbOk = new JButton(Recursos.get("jdsobre_ok"));
+    jpOk.setBackground(Color.WHITE);
+    jbOk.setPreferredSize(new Dimension(100, 27));
+    jpOk.add(jbOk);
+
     add(jpSobre, BorderLayout.CENTER);
+    add(jpOk, BorderLayout.SOUTH);
   }
 }
